d4d6b477e7c687c0c083cd6af1a513415094f77f
cyberjoey <jshelton5@mail.csuchico.edu>
2017-12-20 14:18:08 -0800

    Update textingtree.py

--- textingtree.py
+++ textingtree.py
@@ -35,12 +35,12 @@ def sms():
     rgba = tinycss2.color3.parse_color(sms)
 
     if rgba is None:
-      if sms == "christmas":
+      if (sms == "christmas" or sms == "christmas "):
         rgb_string = '[{0:03d},{1:03d},{2:03d}]'.format(42, 142, 242)
         payload = {'access_token': os.environ['SPARK_ACCESS_TOKEN'], 'command': rgb_string}
         r = requests.post("https://api.spark.io/v1/devices/{0}/color".format(os.environ['SPARK_CORE_ID']), data=payload)
         return Response(mimetype='text/plain')
-      if sms == "maxim":
+      if (sms == "maxim" or sms == "maxim"):
         rgb_string = '[{0:03d},{1:03d},{2:03d}]'.format(42, 142, 243)
         payload = {'access_token': os.environ['SPARK_ACCESS_TOKEN'], 'command': rgb_string}
         r = requests.post("https://api.spark.io/v1/devices/{0}/color".format(os.environ['SPARK_CORE_ID']), data=payload)
